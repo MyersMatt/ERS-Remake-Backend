e00fe7dbad203d5ebed29e724405bc03ff2722a0
Matt Myers <myersmatt1987@gmail.com>
2022-10-06 09:33:08 -0700

    Initial Commit

---
+++ src/main/java/com/practice/ersbackend/ErsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.ersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErsBackendApplication.class, args);
+    }
+
+}
